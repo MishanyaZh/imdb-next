20435991c9abd42dbcb07e55a04eb33b3f010e4c
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2024-06-25 08:10:31 +0200

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Providers from "./Providers";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Providers>
           <Header />
+          <Navbar />
           {children}
         </Providers>
       </body>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavbarItem from "./NavbarItem";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <div className="flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-6">
+      <NavbarItem title="Trending" param="fetchTrending" />
+      <NavbarItem title="Top Rated" param="fetchTopRated" />
+    </div>
+  );
+};
+
+export default Navbar;
