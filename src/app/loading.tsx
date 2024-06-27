d6f6f4288205c5957ea2220d2de8d125a882ee52
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2024-06-27 21:59:53 +0200

    loading, spinner, fix fetch data

---
+++ src/app/loading.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import React from "react";
+
+type Props = {};
+
+const loading = (props: Props) => {
+  return (
+    <div className="flex justify-center mt-16">
+      <Image height={200} width={200} src="spinner.svg" alt="loading..." />
+    </div>
+  );
+};
+
+export default loading;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,25 +8,36 @@ type Props = { searchParams: Params };
 const Home = async ({ searchParams }: Props) => {
   const genre = searchParams.genre || "fetchTrending";
 
-  const res = await fetch(
-    `https://api.themoviedb.org/3${
-      genre === "fetchTopRated" ? `/movie/top_rated` : `trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`
-  );
+  try {
+    const res = await fetch(
+      `https://api.themoviedb.org/3${
+        genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+      }?api_key=${API_KEY}&language=en-US&page=1`,
+      { next: { revalidate: 10000 } }
+    );
 
-  const data = await res.json();
+    if (!res.ok) {
+      throw new Error(`Failed to fetch data: ${res.status} ${res.statusText}`);
+    }
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
+    const text = await res.text();
+    const data = JSON.parse(text);
+
+    const results = data.results;
+
+    return (
+      <div>
+        <Results results={results} />
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return (
+      <div>
+        <p>Error fetching data</p>
+      </div>
+    );
   }
-
-  const results = data.results;
-
-  return (
-    <div>
-      <Results results={results} />
-    </div>
-  );
 };
 
 export default Home;
